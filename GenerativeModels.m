fbb73b9381103a5f0f8c361f8983dbe2194f3459
Dániel Barabási <danielbarabasi@gmail.com>
2021-05-21 17:45:20 -0400

    Revisions to Manuscript

--- GenerativeModels.m
+++ GenerativeModels.m
@@ -137,6 +137,76 @@ set(0,'DefaultFigureWindowStyle','docked')
 title('In Degree Distribution')
 pbaspect([1 1 1])
 
+%% SF with 3 and 0.3 degree exponents
+Base = 12;
+mat = sparse(2^Base,2^Base);
+powOut = repmat('a',Base/3,Base);
+powIn = repmat('a',Base/3,Base);
+edgesAdded = cell(1,Base+1);
+
+for i = 0:Base/3
+    % Set non-2 values different ('0' and '1') for seperate in-hub and out-hub
+    % If non-2 values are same, then the in-hub and out-hub overlap
+    powOut(i+1,:) = strcat(repmat('0',1,i),repmat('2',1,Base-i));
+    powIn(i+1,:) = strcat(repmat('2',1,3*i),repmat('0',1,Base-3*i));
+end
+
+for i = 1:size(powOut,1)
+    edgesAdded{i} = zeros(2,2^(i-1)*2^(Base+1-i));
+    ops2 = Expand([powOut(i,:),powIn(i,:)]);
+    [mat,edgesAdded{i}] = ConnectBarcodesSameMat(ops2(:,1:Base),ops2(:,Base+1:2*Base),mat,edgesAdded{i});
+end
+outDeg = full(sum(mat~=0,2));
+inDeg = full(sum(mat~=0,1));
+figure;
+subplot(1,3,1)
+spy(mat)
+title('Adjacency Matrix')
+
+% Outdegree Distribution
+subplot(1,3,2)
+r = unique(outDeg);
+q = histc(outDeg, unique(outDeg))/2^Base;
+scatter(r,q);
+plotx = r;
+hold on
+p = polyfit(log10(r(1:length(r)-1)),log10(q(1:length(q)-1)),1);
+m = p(1);
+m
+b = 10^(p(2));
+ezplot(@(plotx) b*plotx.^m,[plotx(1) plotx(end)])
+xlim([min(r),max(r)])
+ylim([min(q),max(q)])
+set(gca, 'xScale', 'log')
+set(gca, 'yScale', 'log')
+xlabel('k_{out}','FontSize',14)
+ylabel('p(k_{out})','FontSize',14)
+title('Out Degree Distribution')
+pbaspect([1 1 1])
+
+% Indegree Distribution
+subplot(1,3,3)
+r = unique(inDeg);
+q = histc(inDeg, unique(inDeg))/2^Base;
+scatter(r,q);
+plotx = r;
+hold on
+p = polyfit(log10(r(1:length(r)-1)),log10(q(1:length(q)-1)),1);
+m = p(1);
+m
+b = 10^(p(2));
+b
+ezplot(@(plotx) b*plotx.^m,[plotx(1) plotx(end)])
+xlim([min(r),max(r)])
+ylim([min(q),max(q)])
+set(gca, 'xScale', 'log')
+set(gca, 'yScale', 'log')
+xlabel('k_{in}','FontSize',14)
+ylabel('p(k_{in})','FontSize',14)
+set(0,'DefaultFigureWindowStyle','docked')
+title('In Degree Distribution')
+pbaspect([1 1 1])
+
 %% Binary Tree
 Depth = 4;
 b = Depth;
